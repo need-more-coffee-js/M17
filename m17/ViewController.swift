0abd9ba562a0dec202d3d417d38a8f3750d0dc17
denis <bluesky171@mail.ru>
2024-12-24 15:53:07 +0300

    Initial Commit

---
+++ m17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m17
+//
+//  Created by Денис Ефименков on 24.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
